fae594a05b54268ec176bd4dfc6867633d5cae04
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-04-20 20:49:57 +0330

    Update test.py

--- test.py
+++ test.py
@@ -55,7 +55,7 @@ class Test:
         
         # تعریف transform برای تست
         transform_test = transforms.Compose([
-            transforms.Resize((224, 224)),
+            transforms.Resize((300, 300)),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
