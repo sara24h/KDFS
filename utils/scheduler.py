ba014bd608c86e1393499fb81c76e220f29e2657
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-04-21 22:51:46 +0330

    Update scheduler.py

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -1,98 +1,20 @@
-from torch.optim.lr_scheduler import CosineAnnealingLR
-import warnings
-import math
-
-
-class CosineAnnealingLRWarmup(CosineAnnealingLR):
-    def __init__(
-        self,
-        optimizer,
-        T_max,
-        eta_min=1.0e-5,
-        last_epoch=-1,
-        verbose=False,
-        warmup_steps=2,
-        warmup_start_lr=1.0e-5,
-    ):
-        super(CosineAnnealingLRWarmup, self).__init__(
-            optimizer,
-            T_max=T_max,
-            eta_min=eta_min,
-            last_epoch=last_epoch,
-            verbose=verbose,
-        )
-        self.warmup_steps = warmup_steps
-        self.warmup_start_lr = warmup_start_lr
-        if warmup_steps > 0:
-            self.base_warup_factors = [
-                (base_lr / warmup_start_lr) ** (1.0 / self.warmup_steps)
-                for base_lr in self.base_lrs
-            ]
-
-    def get_lr(self):
-        if not self._get_lr_called_within_step:
-            warnings.warn(
-                "To get the last learning rate computed by the scheduler, "
-                "please use `get_last_lr()`.",
-                UserWarning,
-            )
-        return self._get_closed_form_lr()
-
-    def _get_closed_form_lr(self):
-        if hasattr(self, "warmup_steps"):
-            if self.last_epoch < self.warmup_steps:
-                return [
-                    self.warmup_start_lr * (warmup_factor**self.last_epoch)
-                    for warmup_factor in self.base_warup_factors
-                ]
-            else:
-                return [
-                    self.eta_min
-                    + (base_lr - self.eta_min)
-                    * (
-                        1
-                        + math.cos(
-                            math.pi
-                            * (self.last_epoch - self.warmup_steps)
-                            / (self.T_max - self.warmup_steps)
-                        )
-                    )
-                    * 0.5
-                    for base_lr in self.base_lrs
-                ]
-        else:
-            return [
-                self.eta_min
-                + (base_lr - self.eta_min)
-                * (1 + math.cos(math.pi * self.last_epoch / self.T_max))
-                / 2
-                for base_lr in self.base_lrs
-            ]
-
-
-
 import torch.optim.lr_scheduler as lr_scheduler
 
 class CosineAnnealingLRWarmup:
-    def __init__(self, optimizer, T_max, eta_min, last_epoch, warmup_steps, warmup_start_lr):
-        self.scheduler = lr_scheduler.CosineAnnealingLR(optimizer, T_max, eta_min, last_epoch)
+    def __init__(self, optimizer, T_max, eta_min=0, warmup_steps=0, warmup_start_lr=0):
+        self.optimizer = optimizer
+        self.T_max = T_max
+        self.eta_min = eta_min
         self.warmup_steps = warmup_steps
         self.warmup_start_lr = warmup_start_lr
-        self.optimizer = optimizer
         self.current_step = 0
+        self.scheduler = lr_scheduler.CosineAnnealingLR(optimizer, T_max, eta_min=eta_min)
 
     def step(self):
-        if self.current_step < self.warmup_steps:
-            lr = self.warmup_start_lr + (self.optimizer.param_groups[0]['lr'] - self.warmup_start_lr) * (
-                self.current_step / self.warmup_steps)
+        self.current_step += 1
+        if self.current_step <= self.warmup_steps:
+            lr = self.warmup_start_lr + (self.optimizer.param_groups[0]['lr'] - self.warmup_start_lr) * self.current_step / self.warmup_steps
             for param_group in self.optimizer.param_groups:
                 param_group['lr'] = lr
-            self.current_step += 1
         else:
             self.scheduler.step()
-
-    def state_dict(self):
-        return self.scheduler.state_dict()
-
-    def load_state_dict(self, state_dict):
-        self.scheduler.load_state_dict(state_dict)
