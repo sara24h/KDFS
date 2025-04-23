968e09cc02b81eab73e2a94cf1e5dc14c1b63bf1
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-04-23 15:44:17 +0330

    Update run_resnet50_imagenet_prune1.sh

--- run/run_resnet50_imagenet/run_resnet50_imagenet_prune1.sh
+++ run/run_resnet50_imagenet/run_resnet50_imagenet_prune1.sh
@@ -1,29 +1,58 @@
 arch=ResNet_50
-result_dir=results/run_resnet50_hardfakevsrealfaces
-dataset_dir=/kaggle/input/hardfakevsrealfaces
-dataset_type=hardfakevsrealfaces
-csv_file=/kaggle/input/hardfakevsrealfaces/data.csv
-teacher_dir=/kaggle/working/KDFS/teacher_dir
-
-CUDA_VISIBLE_DEVICES=0 python main.py \
+result_dir=result/run_resnet50_imagenet_prune1
+dataset_dir=dataset_imagenet
+dataset_type=imagenet
+teacher_ckpt_path=teacher_dir/teacher_model.pth
+device=0,1,2,3
+master_port=6681
+CUDA_VISIBLE_DEVICES=$device torchrun --nproc_per_node=4 --master_port $master_port main.py \
 --phase train \
 --dataset_dir $dataset_dir \
 --dataset_type $dataset_type \
---num_workers 4 \
---csv_file /kaggle/input/hardfakevsrealfaces/data.csv \
---teacher_dir /kaggle/working/KDFS/teacher_dir \
---teacher_ckpt_path /kaggle/working/KDFS/teacher_dir/teacher_model.pth \
+--num_workers 8 \
 --pin_memory \
 --device cuda \
 --arch $arch \
---seed 42 \
+--seed 3407 \
 --result_dir $result_dir \
---num_epochs 50 \
---lr 0.001 \
---warmup_steps 5 \
---warmup_start_lr 0.0001 \
---lr_decay_T_max 50 \
---lr_decay_eta_min 0.00001 \
---weight_decay 5e-4 \
---train_batch_size 32 \
---eval_batch_size 32
+--teacher_ckpt_path $teacher_ckpt_path \
+--num_epochs 250 \
+--lr 4e-3 \
+--warmup_steps 10 \
+--warmup_start_lr 4e-5 \
+--lr_decay_T_max 250 \
+--lr_decay_eta_min 4e-5 \
+--weight_decay 2e-5 \
+--train_batch_size 256 \
+--eval_batch_size 256 \
+--target_temperature 3 \
+--gumbel_start_temperature 1 \
+--gumbel_end_temperature 0.1 \
+--coef_kdloss 0.05 \
+--coef_rcloss 1000 \
+--coef_maskloss 10000 \
+--compress_rate 0.68 \
+--ddp \
+&& \
+CUDA_VISIBLE_DEVICES=$device torchrun --nproc_per_node=4 --master_port $master_port main.py \
+--phase finetune \
+--dataset_dir $dataset_dir \
+--dataset_type $dataset_type \
+--num_workers 8 \
+--pin_memory \
+--device cuda \
+--arch $arch \
+--seed 3407 \
+--result_dir $result_dir \
+--finetune_student_ckpt_path $result_dir"/student_model/"$arch"_sparse_last.pt" \
+--finetune_num_epochs 20 \
+--finetune_lr 4e-6 \
+--finetune_warmup_steps 5 \
+--finetune_warmup_start_lr 4e-8 \
+--finetune_lr_decay_T_max 20 \
+--finetune_lr_decay_eta_min 4e-8 \
+--finetune_weight_decay 2e-5 \
+--finetune_train_batch_size 256 \
+--finetune_eval_batch_size 256 \
+--sparsed_student_ckpt_path $result_dir"/student_model/finetune_"$arch"_sparse_best.pt" \
+--ddp \
