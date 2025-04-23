30010f156e654c5beec9a3be28f06c49375abd54
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-04-23 16:11:33 +0330

    Update ResNet.py

--- model/teacher/ResNet.py
+++ model/teacher/ResNet.py
@@ -1,43 +1,24 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
-
 class BasicBlock(nn.Module):
     expansion = 1
 
     def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(
-            in_planes,
-            planes,
-            kernel_size=3,
-            stride=stride,
-            padding=1,
-            bias=False,
-        )
+            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(
-            planes,
-            planes,
-            kernel_size=3,
-            stride=1,
-            padding=1,
-            bias=False,
-        )
+            planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
 
         self.downsample = nn.Sequential()
         if stride != 1 or in_planes != self.expansion * planes:
             self.downsample = nn.Sequential(
                 nn.Conv2d(
-                    in_planes,
-                    self.expansion * planes,
-                    kernel_size=1,
-                    stride=stride,
-                    bias=False,
-                ),
-                nn.BatchNorm2d(self.expansion * planes),
-            )
+                    in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion * planes))
 
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
@@ -45,7 +26,6 @@ class BasicBlock(nn.Module):
         out += self.downsample(x)
         out = F.relu(out)
         return out
-
 
 class Bottleneck(nn.Module):
     expansion = 4
@@ -55,34 +35,18 @@ class Bottleneck(nn.Module):
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(
-            planes,
-            planes,
-            kernel_size=3,
-            stride=stride,
-            padding=1,
-            bias=False,
-        )
+            planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
         self.conv3 = nn.Conv2d(
-            planes,
-            self.expansion * planes,
-            kernel_size=1,
-            bias=False,
-        )
+            planes, self.expansion * planes, kernel_size=1, bias=False)
         self.bn3 = nn.BatchNorm2d(self.expansion * planes)
 
         self.downsample = nn.Sequential()
         if stride != 1 or in_planes != self.expansion * planes:
             self.downsample = nn.Sequential(
                 nn.Conv2d(
-                    in_planes,
-                    self.expansion * planes,
-                    kernel_size=1,
-                    stride=stride,
-                    bias=False,
-                ),
-                nn.BatchNorm2d(self.expansion * planes),
-            )
+                    in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion * planes))
 
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
@@ -92,9 +56,8 @@ class Bottleneck(nn.Module):
         out = F.relu(out)
         return out
 
-
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=2):
+    def __init__(self, block, num_blocks, num_classes=1):  # تغییر به num_classes=1
         super(ResNet, self).__init__()
         self.in_planes = 64
 
@@ -108,18 +71,13 @@ class ResNet(nn.Module):
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
         self.avgpool = nn.Sequential(nn.AvgPool2d(7))
         self.fc = nn.Linear(512 * block.expansion, num_classes)
+        self.sigmoid = nn.Sigmoid()  # اضافه کردن Sigmoid
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1] * (num_blocks - 1)
         layers = []
         for stride in strides:
-            layers.append(
-                block(
-                    self.in_planes,
-                    planes,
-                    stride,
-                )
-            )
+            layers.append(block(self.in_planes, planes, stride))
             self.in_planes = planes * block.expansion
         return nn.Sequential(*layers)
 
@@ -148,16 +106,14 @@ class ResNet(nn.Module):
         out = self.avgpool(out)
         out = out.view(out.size(0), -1)
         out = self.fc(out)
+        out = self.sigmoid(out)  # اعمال Sigmoid
         return out, feature_list
 
-
 def ResNet_18_imagenet():
-    return ResNet(block=BasicBlock, num_blocks=[2, 2, 2, 2], num_classes=2)
-
+    return ResNet(block=BasicBlock, num_blocks=[2, 2, 2, 2], num_classes=1)
 
 def ResNet_34_imagenet():
-    return ResNet(block=BasicBlock, num_blocks=[3, 4, 6, 3], num_classes=2)
-
+    return ResNet(block=BasicBlock, num_blocks=[3, 4, 6, 3], num_classes=1)
 
 def ResNet_50_imagenet():
-    return ResNet(block=Bottleneck, num_blocks=[3, 4, 6, 3], num_classes=2)
+    return ResNet(block=Bottleneck, num_blocks=[3, 4, 6, 3], num_classes=1)
