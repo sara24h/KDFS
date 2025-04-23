659b4c2a1fe2f36e261a47508d2988530f6a2d01
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-04-23 19:16:54 +0330

    Update run_resnet50_imagenet_prune1.sh

--- run/run_resnet50_imagenet/run_resnet50_imagenet_prune1.sh
+++ run/run_resnet50_imagenet/run_resnet50_imagenet_prune1.sh
@@ -1,58 +1,64 @@
+#!/bin/bash
+
+# Model and dataset configurations
 arch=ResNet_50
 result_dir=result/run_resnet50_imagenet_prune1
-dataset_dir=dataset_imagenet
-dataset_type=imagenet
-teacher_ckpt_path=teacher_dir/teacher_model.pth
-device=0,1,2,3
-master_port=6681
-CUDA_VISIBLE_DEVICES=$device torchrun --nproc_per_node=4 --master_port $master_port main.py \
---phase train \
---dataset_dir $dataset_dir \
---dataset_type $dataset_type \
---num_workers 8 \
---pin_memory \
---device cuda \
---arch $arch \
---seed 3407 \
---result_dir $result_dir \
---teacher_ckpt_path $teacher_ckpt_path \
---num_epochs 250 \
---lr 4e-3 \
---warmup_steps 10 \
---warmup_start_lr 4e-5 \
---lr_decay_T_max 250 \
---lr_decay_eta_min 4e-5 \
---weight_decay 2e-5 \
---train_batch_size 256 \
---eval_batch_size 256 \
---target_temperature 3 \
---gumbel_start_temperature 1 \
---gumbel_end_temperature 0.1 \
---coef_kdloss 0.05 \
---coef_rcloss 1000 \
---coef_maskloss 10000 \
---compress_rate 0.68 \
---ddp \
+dataset_dir=/kaggle/input/hardfakevsrealfaces
+dataset_type=hardfakevsrealfaces
+csv_file=/kaggle/input/hardfakevsrealfaces/data.csv
+teacher_ckpt_path=/kaggle/working/KDFS/teacher_dir/teacher_model.pth
+device=cuda
+
+# Run training
+python /kaggle/working/KDFS/main.py \
+    --phase train \
+    --dataset_dir $dataset_dir \
+    --dataset_type $dataset_type \
+    --csv_file $csv_file \
+    --num_workers 8 \
+    --pin_memory \
+    --device $device \
+    --arch $arch \
+    --seed 3407 \
+    --result_dir $result_dir \
+    --teacher_ckpt_path $teacher_ckpt_path \
+    --num_epochs 3 \
+    --lr 5e-4 \
+    --warmup_steps 30 \
+    --warmup_start_lr 1e-4 \
+    --lr_decay_T_max 350 \
+    --lr_decay_eta_min 5e-6 \
+    --weight_decay 1e-4 \
+    --train_batch_size 256 \
+    --eval_batch_size 256 \
+    --target_temperature 3 \
+    --gumbel_start_temperature 2 \
+    --gumbel_end_temperature 0.1 \
+    --coef_kdloss 0.5 \
+    --coef_rcloss 100 \
+    --coef_maskloss 1.0 \
+    --compress_rate 0.68 \
 && \
-CUDA_VISIBLE_DEVICES=$device torchrun --nproc_per_node=4 --master_port $master_port main.py \
---phase finetune \
---dataset_dir $dataset_dir \
---dataset_type $dataset_type \
---num_workers 8 \
---pin_memory \
---device cuda \
---arch $arch \
---seed 3407 \
---result_dir $result_dir \
---finetune_student_ckpt_path $result_dir"/student_model/"$arch"_sparse_last.pt" \
---finetune_num_epochs 20 \
---finetune_lr 4e-6 \
---finetune_warmup_steps 5 \
---finetune_warmup_start_lr 4e-8 \
---finetune_lr_decay_T_max 20 \
---finetune_lr_decay_eta_min 4e-8 \
---finetune_weight_decay 2e-5 \
---finetune_train_batch_size 256 \
---finetune_eval_batch_size 256 \
---sparsed_student_ckpt_path $result_dir"/student_model/finetune_"$arch"_sparse_best.pt" \
---ddp \
+# Run finetuning
+python /kaggle/working/KDFS/main.py \
+    --phase finetune \
+    --dataset_dir $dataset_dir \
+    --dataset_type $dataset_type \
+    --csv_file $csv_file \
+    --num_workers 8 \
+    --pin_memory \
+    --device $device \
+    --arch $arch \
+    --seed 3407 \
+    --result_dir $result_dir \
+    --finetune_student_ckpt_path $result_dir"/student_model/"$arch"_sparse_last.pt" \
+    --finetune_num_epochs 20 \
+    --finetune_lr 4e-6 \
+    --finetune_warmup_steps 5 \
+    --finetune_warmup_start_lr 4e-8 \
+    --finetune_lr_decay_T_max 20 \
+    --finetune_lr_decay_eta_min 4e-8 \
+    --weight_decay 2e-5 \
+    --finetune_train_batch_size 256 \
+    --finetune_eval_batch_size 256 \
+    --sparsed_student_ckpt_path $result_dir"/student_model/finetune_"$arch"_sparse_best.pt"
