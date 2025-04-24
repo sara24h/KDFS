3c2b8ca32e94767def3ef8e7d5a2677a496b2acd
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-04-24 18:39:49 +0330

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,37 +1,46 @@
-import logging
 import os
+import sys
+import shutil
+import time, datetime
+import logging
+import numpy as np
+from pathlib import Path
+import time, datetime
 import re
 import torch
 
-def get_logger(logpath, name):
+
+def record_config(args, path):
+    now = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+    if args.resume:
+        with open(path, "a") as f:
+            f.write(now + "\n\n")
+            for arg in vars(args):
+                f.write("{}: {}\n".format(arg, getattr(args, arg)))
+            f.write("\n")
+    else:
+        with open(path, "w") as f:
+            f.write(now + "\n\n")
+            for arg in vars(args):
+                f.write("{}: {}\n".format(arg, getattr(args, arg)))
+            f.write("\n")
+
+
+def get_logger(file_path, name):
     logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
-    file_handler = logging.FileHandler(logpath)
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    log_format = "%(asctime)s | %(message)s"
+    formatter = logging.Formatter(log_format, datefmt="%m/%d %I:%M:%S %p")
+    file_handler = logging.FileHandler(file_path)
     file_handler.setFormatter(formatter)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+
     logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+    logger.setLevel(logging.INFO)
+
     return logger
 
-def record_config(args, filepath):
-    with open(filepath, 'w') as f:
-        for key, value in vars(args).items():
-            f.write(f"{key}: {value}\n")
-
-def get_accuracy(output, target, topk=(1,)):
-    num_classes = output.size(1)  # تعداد کلاس‌های خروجی
-    topk = [k for k in topk if k <= num_classes]  # فیلتر کردن مقادیر نامعتبر
-    if not topk:
-        topk = [1]  # حداقل top-1
-    maxk = max(topk)
-    batch_size = target.size(0)
-    _, pred = output.topk(maxk, 1, True, True)
-    pred = pred.t()
-    correct = pred.eq(target.view(1, -1).expand_as(pred))
-    res = []
-    for k in topk:
-        correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
-        res.append(correct_k.mul_(100.0 / batch_size))
-    return res
 
 def get_compress_rates(cprate_str):
     cprate_str_list = cprate_str.split("+")
@@ -48,3 +57,20 @@ def get_compress_rates(cprate_str):
         assert len(find_cprate) == 1
         cprate += [float(find_cprate[0])] * num
     return cprate
+
+
+def get_accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].contiguous().view(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res[0]  # فقط Top-1 را برگردانید
