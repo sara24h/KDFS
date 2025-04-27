db02773b88f6933ace43e81aa5242029440651ff
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-04-28 00:25:19 +0330

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -40,13 +40,14 @@ class Dataset_hardfakevsreal(Dataset):
    
     def get_train_transform():
         return transforms.Compose([
-            transforms.RandomResizedCrop(300, scale=(0.8, 1.0)),
+            transforms.RandomCrop(300, padding=30),
             transforms.RandomHorizontalFlip(p=0.5),
             transforms.RandomRotation(15),
             transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.1),
             transforms.RandomErasing(p=0.3, scale=(0.02, 0.33)),
             transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            transforms.Normalize([0.485, 0.456, 0.406],
+                                 [0.229, 0.224, 0.225])
         ])
 
     @staticmethod
