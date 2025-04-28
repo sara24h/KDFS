cf82d2c837f9b1ee361db7f0d8f31cc2f962d662
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-04-28 23:27:40 +0330

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -38,9 +38,12 @@ import torch
              transforms.RandomHorizontalFlip(p=0.5),
              transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
              transforms.RandomAffine(degrees=10, translate=(0.1, 0.1), scale=(0.9, 1.1)),
+             transforms.RandomErasing(p=0.5, scale=(0.02, 0.2)),
+             transforms.ToTensor(),
+             transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
              transforms.ToTensor(), 
              transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
-          
+             transforms.RandomErasing(p=0.5, scale=(0.02, 0.2)),  
          ])
  
          transform_test = transforms.Compose([
