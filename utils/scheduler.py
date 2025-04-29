429060e96347c2b34354314195516328400fbb6b
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-04-29 15:54:15 +0330

    Update scheduler.py

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -1,35 +1,70 @@
-import torch.optim.lr_scheduler as lr_scheduler
+from torch.optim.lr_scheduler import CosineAnnealingLR
+import warnings
 import math
 
-class CosineAnnealingLRWarmup:
-    def __init__(self, optimizer, T_max, eta_min=0, warmup_steps=0, warmup_start_lr=0):
-        self.optimizer = optimizer
-        self.T_max = T_max
-        self.eta_min = eta_min
+
+class CosineAnnealingLRWarmup(CosineAnnealingLR):
+    def __init__(
+        self,
+        optimizer,
+        T_max,
+        eta_min=1.0e-5,
+        last_epoch=-1,
+        verbose=False,
+        warmup_steps=2,
+        warmup_start_lr=1.0e-5,
+    ):
+        super(CosineAnnealingLRWarmup, self).__init__(
+            optimizer,
+            T_max=T_max,
+            eta_min=eta_min,
+            last_epoch=last_epoch,
+            verbose=verbose,
+        )
         self.warmup_steps = warmup_steps
         self.warmup_start_lr = warmup_start_lr
-        self.current_step = 0
-        self.base_lrs = [group['lr'] for group in optimizer.param_groups]
-        self.cosine_scheduler = lr_scheduler.CosineAnnealingLR(optimizer, T_max, eta_min=eta_min)
+        if warmup_steps > 0:
+            self.base_warup_factors = [
+                (base_lr / warmup_start_lr) ** (1.0 / self.warmup_steps)
+                for base_lr in self.base_lrs
+            ]
 
-    def step(self):
-        self.current_step += 1
-        if self.current_step <= self.warmup_steps and self.warmup_steps > 0:
-            # Warmup phase
-            for param_group, base_lr in zip(self.optimizer.param_groups, self.base_lrs):
-                fraction = self.current_step / self.warmup_steps
-                lr = self.warmup_start_lr + fraction * (base_lr - self.warmup_start_lr)
-                param_group['lr'] = lr
+    def get_lr(self):
+        if not self._get_lr_called_within_step:
+            warnings.warn(
+                "To get the last learning rate computed by the scheduler, "
+                "please use `get_last_lr()`.",
+                UserWarning,
+            )
+        return self._get_closed_form_lr()
+
+    def _get_closed_form_lr(self):
+        if hasattr(self, "warmup_steps"):
+            if self.last_epoch < self.warmup_steps:
+                return [
+                    self.warmup_start_lr * (warmup_factor**self.last_epoch)
+                    for warmup_factor in self.base_warup_factors
+                ]
+            else:
+                return [
+                    self.eta_min
+                    + (base_lr - self.eta_min)
+                    * (
+                        1
+                        + math.cos(
+                            math.pi
+                            * (self.last_epoch - self.warmup_steps)
+                            / (self.T_max - self.warmup_steps)
+                        )
+                    )
+                    * 0.5
+                    for base_lr in self.base_lrs
+                ]
         else:
-            # Cosine annealing phase
-            self.cosine_scheduler.step(self.current_step - self.warmup_steps)
-
-    def state_dict(self):
-        return {
-            'current_step': self.current_step,
-            'cosine_scheduler': self.cosine_scheduler.state_dict()
-        }
-
-    def load_state_dict(self, state_dict):
-        self.current_step = state_dict['current_step']
-        self.cosine_scheduler.load_state_dict(state_dict['cosine_scheduler'])
+            return [
+                self.eta_min
+                + (base_lr - self.eta_min)
+                * (1 + math.cos(math.pi * self.last_epoch / self.T_max))
+                / 2
+                for base_lr in self.base_lrs
+            ]
