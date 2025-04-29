d0d2ad53203c5357e3ea25a4ce98f61338c61c04
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-04-29 16:38:31 +0330

    Update main.py

--- main.py
+++ main.py
@@ -20,9 +20,7 @@ from utils import utils, loss, meter, scheduler
 import json
 import time
 from test import Test
-from finetune import Finetune
-import torch.distributed as dist
-from train import Train
+
 
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
 
@@ -31,7 +29,7 @@ matplotlib.use('Agg')
 
 
 def parse_args():
-    desc = "Pytorch implementation of KDFS for hardfakevsreal dataset"
+    desc = "Pytorch implementation of KDFS"
     parser = argparse.ArgumentParser(description=desc)
 
     parser.add_argument(
@@ -42,38 +40,26 @@ def parse_args():
         help="train, finetune or test",
     )
 
-
     parser.add_argument(
-        "--csv_file", 
-        type=str, 
-        default="/kaggle/input/hardfakevsrealfaces/data.csv", 
-        help="The path to the CSV file"
-    )
-  
-    parser.add_argument(
-        "--dataset_dir",
-        type=str,
-        default="/kaggle/input/hardfakevsrealfaces",
-        help="The directory containing the dataset",
-    )
-    parser.add_argument(
-         "--root_dir", 
-         type=str, 
-         default="/kaggle/input/hardfakevsrealfaces", 
-         help="The directory containing the images",
-     
+        "--dataset_dir", type=str, default="/kaggle/input/hardfakevsrealfaces", help="The dataset path"
     )
     parser.add_argument(
         "--dataset_type",
         type=str,
         default="hardfakevsreal",
-        choices=("hardfakevsreal",),
+        choices=("cifar10", "cifar100", "imagenet", "hardfakevsreal"),
         help="The type of dataset",
+    )
+    parser.add_argument(
+        "--csv_file",
+        type=str,
+        default="/kaggle/input/hardfakevsrealfaces/data.csv",
+        help="The path to the CSV file",
     )
     parser.add_argument(
         "--num_workers",
         type=int,
-        default=8,
+        default=4,
         help="The num_workers of dataloader",
     )
     parser.add_argument(
@@ -82,10 +68,26 @@ def parse_args():
         help="The pin_memory of dataloader",
     )
     parser.add_argument(
+        "--resume",
+        type=str,
+        default=None,
+        help="load the model from the specified checkpoint",
+    )
+    parser.add_argument("--ddp", action="store_true", help="Use the distributed data parallel")
+    parser.add_argument(
         "--arch",
         type=str,
         default="ResNet_50",
-        choices=("ResNet_50",),
+        choices=(
+            "ResNet_18",
+            "ResNet_50",
+            "VGG_16_bn",
+            "resnet_56",
+            "resnet_110",
+            "DenseNet_40",
+            "GoogLeNet",
+            "MobileNetV2",
+        ),
         help="The architecture to prune",
     )
     parser.add_argument(
@@ -95,22 +97,12 @@ def parse_args():
         choices=("cuda", "cpu"),
         help="Device to use",
     )
-    parser.add_argument(
-        "--seed", 
-        type=int, 
-        default=3407, 
-        help="Init seed"
-    )
+    parser.add_argument("--seed", type=int, default=3407, help="Init seed")
     parser.add_argument(
         "--result_dir",
         type=str,
-        default="./result_hardfakevsreal/",
+        default="./result/",
         help="The directory where the results will be stored",
-    )
-    parser.add_argument(
-        "--ddp",
-        action="store_true",
-        help="Use the distributed data parallel",
     )
     parser.add_argument(
         "--dali",
@@ -118,24 +110,17 @@ def parse_args():
         help="Use dali",
     )
 
-
     parser.add_argument(
         "--teacher_ckpt_path",
         type=str,
-        default=None,
-        help="The path where to load the teacher ckpt",
+        default="/kaggle/working/KDFS/teacher_dir/teacher_model.pth",
+        help="The path where the teacher model is stored",
     )
     parser.add_argument(
-        "--num_epochs", 
-        type=int, 
-        default=350, 
-        help="The num of epochs to train."
+        "--num_epochs", type=int, default=3, help="The num of epochs to train."
     )
     parser.add_argument(
-        "--lr", 
-        default=5e-4, 
-        type=float, 
-        help="The initial learning rate of model"
+        "--lr", default=5e-4, type=float, help="The initial learning rate of model"
     )
     parser.add_argument(
         "--warmup_steps",
@@ -161,23 +146,12 @@ def parse_args():
         type=float,
         help="eta_min of CosineAnnealingLR",
     )
+    parser.add_argument("--weight_decay", type=float, default=1e-4, help="Weight decay")
     parser.add_argument(
-        "--weight_decay", 
-        type=float, 
-        default=1e-4, 
-        help="Weight decay"
+        "--train_batch_size", type=int, default=16, help="Batch size for training"
     )
     parser.add_argument(
-        "--train_batch_size", 
-        type=int, 
-        default=32, 
-        help="Batch size for training"
-    )
-    parser.add_argument(
-        "--eval_batch_size", 
-        type=int, 
-        default=32, 
-        help="Batch size for validation"
+        "--eval_batch_size", type=int, default=16, help="Batch size for validation"
     )
     parser.add_argument(
         "--target_temperature",
@@ -198,10 +172,7 @@ def parse_args():
         help="Gumbel-softmax temperature at the end of training",
     )
     parser.add_argument(
-        "--coef_kdloss", 
-        type=float, 
-        default=0.5, 
-        help="Coefficient of kd loss"
+        "--coef_kdloss", type=float, default=0.5, help="Coefficient of kd loss"
     )
     parser.add_argument(
         "--coef_rcloss",
@@ -210,24 +181,14 @@ def parse_args():
         help="Coefficient of reconstruction loss",
     )
     parser.add_argument(
-        "--coef_maskloss", 
-        type=float, 
-        default=1.0, 
-        help="Coefficient of mask loss"
+        "--coef_maskloss", type=float, default=1.0, help="Coefficient of mask loss"
     )
     parser.add_argument(
         "--compress_rate",
         type=float,
-        default=None,
+        default=0.68,
         help="Compress rate of the student model",
     )
-    parser.add_argument(
-        "--resume",
-        type=str,
-        default=None,
-        help="load the model from the specified checkpoint",
-    )
-
 
     parser.add_argument(
         "--finetune_student_ckpt_path",
@@ -238,77 +199,66 @@ def parse_args():
     parser.add_argument(
         "--finetune_num_epochs",
         type=int,
-        default=100,
+        default=20,
         help="The num of epochs to train in finetune",
     )
     parser.add_argument(
         "--finetune_lr",
-        default=1e-5,
+        default=4e-6,
         type=float,
         help="The initial learning rate of model in finetune",
     )
     parser.add_argument(
         "--finetune_warmup_steps",
-        default=10,
+        default=5,
         type=int,
         help="The steps of warmup in finetune",
     )
     parser.add_argument(
         "--finetune_warmup_start_lr",
-        default=1e-4,
+        default=4e-8,
         type=float,
         help="The steps of warmup in finetune",
     )
     parser.add_argument(
         "--finetune_lr_decay_T_max",
-        default=100,
+        default=20,
         type=int,
         help="T_max of CosineAnnealingLR in finetune",
     )
     parser.add_argument(
         "--finetune_lr_decay_eta_min",
-        default=5e-6,
+        default=4e-8,
         type=float,
         help="eta_min of CosineAnnealingLR in finetune",
     )
     parser.add_argument(
         "--finetune_weight_decay",
         type=float,
-        default=1e-4,
+        default=2e-5,
         help="Weight decay in finetune",
     )
     parser.add_argument(
         "--finetune_train_batch_size",
         type=int,
-        default=32,
+        default=16,
         help="Batch size for training in finetune",
     )
     parser.add_argument(
         "--finetune_eval_batch_size",
         type=int,
-        default=32,
+        default=16,
         help="Batch size for validation in finetune",
     )
-    parser.add_argument(
-        "--finetune_resume",
-        type=str,
-        default=None,
-        help="load the model from the specified checkpoint in finetune",
-    )
-
-    parser.add_argument(
-        "--test_batch_size", 
-        type=int, 
-        default=32, 
-        help="Batch size for test"
-    )
-
-    
     parser.add_argument(
         "--sparsed_student_ckpt_path",
         type=str,
         default=None,
-        help="The path where to load the sparsed student ckpt",
+        help="The path where to save the sparsed student ckpt in finetune",
+    )
+
+    parser.add_argument(
+        "--test_batch_size", type=int, default=16, help="Batch size for test"
     )
 
     return parser.parse_args()
@@ -316,12 +266,7 @@ def parse_args():
 def main():
     args = parse_args()
     if args.ddp:
-        if args.phase == "train":
-            train = TrainDDP(args=args)
-            train.main()
-        elif args.phase == "finetune":
-            finetune = FinetuneDDP(args=args)
-            finetune.main()
+        raise NotImplementedError("Distributed Data Parallel (DDP) is not implemented in this version.")
     else:
         if args.phase == "train":
             train = Train(args=args)
