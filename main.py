35b5b25f1866aacb58c9baac2a744d1ee5e434d7
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-04-29 15:41:30 +0330

    Update main.py

--- main.py
+++ main.py
@@ -56,6 +56,13 @@ def parse_args():
         default="/kaggle/input/hardfakevsrealfaces",
         help="The directory containing the dataset",
     )
+     parser.add_argument(
+         "--root_dir", 
+         type=str, 
+         default="/kaggle/input/hardfakevsrealfaces", 
+         help="The directory containing the images",
+     
+     )
     parser.add_argument(
         "--dataset_type",
         type=str,
