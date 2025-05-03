4e7acdb286ebfe14a8bfe0eec40d7ce97a4c1da5
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-05-03 12:11:38 +0330

    Update loss.py

--- utils/loss.py
+++ utils/loss.py
@@ -1,19 +1,16 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.utils
 
 
 class KDLoss(nn.Module):
     def __init__(self):
         super(KDLoss, self).__init__()
+        self.bce_loss = nn.BCEWithLogitsLoss()
 
     def forward(self, logits_t, logits_s):
-        return F.kl_div(
-            F.log_softmax(logits_s, dim=1),
-            F.softmax(logits_t, dim=1),
-            reduction="batchmean",
-        )
+        # برای خروجی باینری، از BCEWithLogitsLoss استفاده می‌کنیم
+        return self.bce_loss(logits_s, torch.sigmoid(logits_t))  # logits_t به احتمال تبدیل می‌شه
 
 
 class RCLoss(nn.Module):
