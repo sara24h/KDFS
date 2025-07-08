7141984e8949149e3f4b0862fa11713e22f41eba
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-07-09 00:25:11 +0330

    Update train_ddp.py

--- train_ddp.py
+++ train_ddp.py
@@ -413,11 +413,11 @@ class TrainDDP:
 
                     scaler.scale(total_loss).backward()
                     
-                     if self.rank == 0:
+                    if self.rank == 0:
                         for i, m in enumerate(self.student.module.mask_modules):
                             if m.mask_weight.grad is not None:
                                 self.logger.info(f"[Grad] Epoch {epoch}, Mask {i} grad norm: {torch.norm(m.mask_weight.grad).item():.4e}")
-                            else:
+                             else:
                                 self.logger.info(f"[Grad] Epoch {epoch}, Mask {i} grad is None")
                     
                     scaler.step(self.optim_weight)
