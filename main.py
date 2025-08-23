21e156915eee7a40f89a27cb62a8e4a7021d4bb1
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-08-23 12:44:33 +0330

    Update main.py

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ from utils import utils, loss, meter, scheduler
 from train import Train
 from test import Test
 from finetune import Finetune
+from finetune_gen import Finetune
 from train_ddp import TrainDDP
 from finetune_ddp import FinetuneDDP
 import json
